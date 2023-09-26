92e38108e8deb3b28fe16c38beff56f2e183ecec
Alex <taisiya.voit@gmail.com>
2023-09-27 01:32:32 +0300

    Resolved

--- Homework5/OOP.py
+++ Homework5/OOP.py
@@ -5,25 +5,20 @@ class Point:
         self.y = y
 
 
-class Circle(Point):
+class Circle:
+
     def __init__(self, x, y, radius):
-        super().__init__(x, y)
+        self.x = x
+        self.y = y
         self.radius = radius
 
     def contains(self):
-        if self.x**2+self.y**2 <= self.radius**2:
+        if self.x ** 2 + self.y ** 2 <= self.radius ** 2:
             return True
         else:
             return False
 
 
-class Circle2:
-    def __init__(self, radius):
-        self.radius = radius
-
-    def contains(self, x, y):
-        if x**2 + y**2 <= self.radius:
-            return True
-        else:
-            return False
-
+p = Point(4, 4)
+c = Circle(p.x, p.y, 5)
+print(c.contains())
